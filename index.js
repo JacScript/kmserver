de379c1504bb6ab579efbf4c2c5abfa7d570d3e3
JacScript <jacksonsylas59@gmail.com>
2025-08-16 13:53:36 -0700

    mc

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ const MONGOURL = process.env.MONGOURL;
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
+  "https://kaimaembe.com/",
+  "https://admin.kaimaembe.com"
   // "http://127.0.0.1:3001",
   // "http://127.0.0.1:5000"
 ];
